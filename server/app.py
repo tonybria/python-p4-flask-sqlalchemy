d3982ff6f826a73543c1810e78eba45afaf88618
tony-brian <btoni3823@gmail.com>
2023-09-26 20:08:52 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+# Configure your database URL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+# Disable tracking modifications for SQLAlchemy (optional but recommended)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize SQLAlchemy and Migrate
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# Uncomment this line if you have models in separate files
+# db.init_app(app)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,8 +1,20 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+db = SQLAlchemy()
 
-db = SQLAlchemy(metadata=metadata)
+class Owner(db.Model):
+    __tablename__ = 'owners'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=True, nullable=False)
+
+class pet(db.Model):
+    __tablename__ = 'pets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=True, nullable=False)
+    species = db.Column(db.String(255), nullable=False)
+
+    owner_id = db.Column(db.Integer, db.ForeignKey('owners.id'), nullable=False)
+    owner = db.relationship('Owner', backref=db.backref('pets', lazy=True))
+
+    def __repr__(self):
+        return f'<Pet {self.name},{self.species}>'
